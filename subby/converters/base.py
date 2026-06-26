7e2bf445cc2a1d4d250a0fa6d2a4d8984a0ba66f
9Oc <1987285+9Oc@users.noreply.github.com>
2026-06-25 21:28:52 -0500

    Update base.py to expose errors kwarg in converter functions

--- subby/converters/base.py
+++ subby/converters/base.py
@@ -9,19 +9,19 @@ from subby.subripfile import SubRipFile
 class BaseConverter(ABC):
     """Base subtitle converter class"""
 
-    def from_file(self, file: Path) -> SubRipFile:
+    def from_file(self, file: Path, errors: str = 'strict') -> SubRipFile:
         """Reads a given file and converts it to srt"""
         with file.open(mode='rb') as stream:
-            return self.parse(stream)
+            return self.parse(stream, errors=errors)
 
-    def from_string(self, data: str) -> SubRipFile:
+    def from_string(self, data: str, errors: str = 'strict') -> SubRipFile:
         """Reads a given string and converts it to srt"""
-        return self.parse(BytesIO(data.encode('utf-8')))
+        return self.parse(BytesIO(data.encode('utf-8')), errors=errors)
 
-    def from_bytes(self, data: bytes) -> SubRipFile:
+    def from_bytes(self, data: bytes, errors: str = 'strict') -> SubRipFile:
         """Parses given data and converts it to srt"""
-        return self.parse(BytesIO(data))
+        return self.parse(BytesIO(data), errors=errors)
 
     @abstractmethod
-    def parse(self, stream: BinaryIO) -> SubRipFile:
+    def parse(self, stream: BinaryIO, errors: str = 'strict') -> SubRipFile:
         """Parses data from a given stream and converts it to srt"""
